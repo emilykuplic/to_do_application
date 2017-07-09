ec752a5ec6d985fd20bb0176a8d44f093a2715e1
Emily Kuplic <emilykuplic@gmail.com>
2017-07-09 15:36:39 -0500

    tasks appending to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,8 @@ console.log( 'js' );
 $( document ).ready( function(){
   console.log( 'JQ' );
 
+getTasks();
+
   $( '#submitTask' ).on( 'click', function(){
       console.log( 'in submitTask on click' );
       // create object out of user input
@@ -25,4 +27,34 @@ function saveTask( newTask){
       console.log( 'A new to do: ', data);
     } // end success
   }); //end ajax
+}
+
+function getTasks(){
+  console.log( 'in getTasks' );
+  // ajax call to server to get tasks
+  $.ajax({
+    url: '/todo',
+    type: 'GET',
+    //sending back an object with an array of objects (tasks)
+    success: function(data){
+      console.log( 'Tasks: ', data );
+      var task = data.task; // Array of task
+      appendToDom(task);
+    } // end success
+  }); //end ajax
+  // display on DOM with buttons that allow edit of each
+} // end getTasks
+
+// Append array of tasks to the DOM
+function appendToDom(task){
+  $('#viewTasks').empty();
+  for(var i = 0; i < task.length; i += 1) {
+    var tasks = task[i];
+    console.log(task[i]);
+    // For each task, append a new row to our table
+    $tr = $('<tr></tr>');
+    $tr.data('task', task);
+    $tr.append('<td>' + tasks.task + '</td>');
+    $('#viewTasks').append($tr);
+}
 }
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -50,7 +50,34 @@ router.post('/', function(req, res) {
   }); // end pool
 });
 
-
+router.get('/', function(req, res){
+  // errorConnecting is bool, db is what we query against,
+  // done is a function that we call when we're done
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.sendStatus(500);
+    } else {
+      // We connected to the database!!!
+      // Now we're going to GET things from the db
+      var queryText = 'SELECT "task" FROM "todo";';
+      // errorMakingQuery is a bool, result is an object
+      db.query(queryText, function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Attempted to query with', queryText);
+          console.log('Error making query');
+          res.sendStatus(500);
+        } else {
+          // console.log(result);
+          // Send back the results
+          var data = {task: result.rows};
+          res.send(data);
+        }
+      }); // end query
+    } // end if
+  }); // end pool
+}); // end of GET
 
 
 
